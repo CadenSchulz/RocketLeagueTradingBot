8c79f81e26716ecd08393d06f5cdcc09d613afca
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-11 17:39:49 -0600

    first

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ client.on('message', message => {
 
 		client.commands.get('help').execute(message, args, Discord)
 
-	if(command === 'giveaway')}
+	if(command === 'giveaway'){
 		client.commands.get('help').execute(message, args, Discord);
 	}
 });
