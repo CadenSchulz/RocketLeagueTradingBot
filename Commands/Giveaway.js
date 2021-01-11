d9e6fd06b823a9bb66f611c2f34a70a43bf8deff
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-11 17:58:19 -0600

    final Giveaway

--- Commands/Giveaway.js
+++ Commands/Giveaway.js
@@ -11,8 +11,7 @@ module.exports = {
             {name: '**Step 2**', value: 'Invite as many people as you can to click the check mark!'},
             {name: '**Step 3**', value: '__ONCE THIS MESSAGE HAS 700 REACTIONS (or check marks clicked) THEN THE OCTANE WILL BE GIVEN AT RANDOM TO A LUCKY WINNER__'}
         )
-        .setFooter('Rocket League Trading Server ©')
-        .setFooterImage('https://img.icons8.com/nolan/452/rocket-league.png');
+        .setFooter('Rocket League Trading Server ©');
 
         message.channel.send(newEmbed);
     }
