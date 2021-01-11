207317e48065e0687eee66122d223a4c57c73e43
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-11 17:51:05 -0600

    yes

--- Commands/Giveaway.js
+++ Commands/Giveaway.js
@@ -4,12 +4,12 @@ module.exports = {
     execute(message, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#777777')
-        .setTitle('**TW Octane Giveaway**')
-        .setDescription('How To Enter:')
+        .setTitle('**__TW Octane Giveaway__**')
+        .setDescription('**How To Enter:**')
         .addFields(
-            {name: 'Step 1', value: 'React to this message (click the check mark)'},
-            {name: 'Step 2', value: 'Invite as many people as you can to click the check mark!'},
-            {name: 'Step 3', value: '**ONCE THIS MESSAGE HAS 700 REACTIONS (or check marks clicked) THEN THE OCTANE WILL BE GIVEN AT RANDOM TO A LUCKY WINNER**'}
+            {name: '**Step 1**', value: 'React to this message (click the check mark)'},
+            {name: '**Step 2**', value: 'Invite as many people as you can to click the check mark!'},
+            {name: '**Step 3**', value: '__ONCE THIS MESSAGE HAS 700 REACTIONS (or check marks clicked) THEN THE OCTANE WILL BE GIVEN AT RANDOM TO A LUCKY WINNER__'}
         )
         .setFooter('Rocket League Trading Server ©');
 
