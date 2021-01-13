042df1ad7f07c5345a74150f0f7b8bdbb4a2ab21
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-13 15:04:32 -0600

    Update server.js

--- Commands/server.js
+++ Commands/server.js
@@ -1,7 +1,28 @@
 module.exports = {
     name: 'server',
-    description: "Embed for server Command",
-    execute(message, args) {
-        message.channel.send(```Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}``);
+    description: "Embed for youtube Command",
+    execute(message, args, Discord){
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#777777')
+        .setTitle(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`)
+        .setDescription("**This is the Rocket League Trading Server's Youtube Channel:** [click me](https://www.youtube.com/channel/UCeHkU3QW6zcEC68F3F4zwXw).")
+        .addFields(
+            {name: 'Step 1', value: 'Like'},
+            {name: 'Step 2', value: 'Subscribe'},
+            {name: 'Step 3', value: 'Comment'}
+        )
+        .setFooter('Rocket League Trading Server ©');
+
+        message.channel.send(newEmbed);
     }
-};
+}
+
+
+
+
+
+
+
+
+
+message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
