4cbb3f788c20ff9f1d5da08c58bdeb0f364b0b18
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-13 15:15:17 -0600

    Update commands.js

--- Commands/commands.js
+++ Commands/commands.js
@@ -9,6 +9,7 @@ module.exports = {
         .addFields(
             {name: '``-help``', value: '``-youtube``'}, //TYPE `` to make the gray outline in the embed
             {name: '``-bot``', value: '**``-ping``**'},
+            {name: '``-server``', value: '**``----------``**'},
             {name: '**Others:**', value: '``Type the word "help"``'},
             {name: 'NEED HELP STILL?', value: '``-commands or -help``'}
         )
