762d88812032f279ba54c98ba21d53a4e1b0d64d
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-12 18:11:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -27,27 +27,6 @@ client.once('ready', () =>{
 
 client.on('message', message => {
 
-	switch (args[0]) {
-		case 'help':
-			const Embed = new RichEmbed()
-			.setColor('#777777')
-        	.setTitle('**Help Center**')
-        	.setDescription('Follow these steps to get help')
-        	.addFields(
-            	{name: 'Step 1', value: 'Go to the SUPPORT channel'},
-         		{name: 'Step 2', value: 'Click the reaction button (looks like an envelope)'},
-            	{name: 'Step 3', value: 'A private channel that only you and moderators can see will be setup. Talk with a moderator and they will help you out!'},
-            	{name: 'Extra Help', value: 'If no moderator assists you, then talk in general-chat for community support'}
-        	)
-        	.setImage('https://img.icons8.com/nolan/452/rocket-league.png')
-        	.setFooter('Rocket League Trading Server ©');
-
-			message.author.send(Embed);
-		break;
-    }
-	}
-
-
 	if(!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
