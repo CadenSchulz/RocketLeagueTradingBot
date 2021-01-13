e1d8571a21c8fc67bb6f209e8406c5467d5fe901
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-13 14:57:01 -0600

    Update server.js

--- Commands/server.js
+++ Commands/server.js
@@ -3,5 +3,5 @@ module.exports = {
     description: "Embed for server Command",
     execute(message, args) {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-        }
-    };
+    }
+};
