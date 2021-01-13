7a60a0d2fd5ba6d9edc6cdcbd8661c4cdc20d727
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-13 12:56:06 -0600

    Update youtube.js

--- Commands/youtube.js
+++ Commands/youtube.js
@@ -5,7 +5,7 @@ module.exports = {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#777777')
         .setTitle('**__Rocket League Trading Server__**')
-        .setDescription['Youtube'](https://www.youtube.com/channel/UCeHkU3QW6zcEC68F3F4zwXw)
+        .setDescription['Youtube']("https://www.youtube.com/channel/UCeHkU3QW6zcEC68F3F4zwXw")
         .addFields(
             {name: 'Step 1', value: 'Like'},
             {name: 'Step 2', value: 'Subscribe'},
