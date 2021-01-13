63b42eaa0341dbac4af547732a0c2dd01634898f
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-13 15:13:18 -0600

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,7 +1,12 @@
 module.exports = {
     name: 'ping',
     description: "Embed for ping Command",
-    execute(message, args) {
-        message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms`);
-        }
-    };
+    execute(message, args, Discord){
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#777777')
+        .setDescription(`🏓Latency is ${Date.now() - message.createdTimestamp}ms`)
+        .setFooter('Rocket League Trading Server ©');
+    
+        message.channel.send(newEmbed);
+    }
+}
