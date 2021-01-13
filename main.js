feaf3c94f5dbb9f40be67888bce10adaa5004643
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-12 18:10:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -31,18 +31,19 @@ client.on('message', message => {
 		case 'help':
 			const Embed = new RichEmbed()
 			.setColor('#777777')
-        .setTitle('**Help Center**')
-        .setDescription('Follow these steps to get help')
-        .addFields(
-            {name: 'Step 1', value: 'Go to the SUPPORT channel'},
-            {name: 'Step 2', value: 'Click the reaction button (looks like an envelope)'},
-            {name: 'Step 3', value: 'A private channel that only you and moderators can see will be setup. Talk with a moderator and they will help you out!'},
-            {name: 'Extra Help', value: 'If no moderator assists you, then talk in general-chat for community support'}
-        )
-        .setImage('https://img.icons8.com/nolan/452/rocket-league.png')
-        .setFooter('Rocket League Trading Server ©');
+        	.setTitle('**Help Center**')
+        	.setDescription('Follow these steps to get help')
+        	.addFields(
+            	{name: 'Step 1', value: 'Go to the SUPPORT channel'},
+         		{name: 'Step 2', value: 'Click the reaction button (looks like an envelope)'},
+            	{name: 'Step 3', value: 'A private channel that only you and moderators can see will be setup. Talk with a moderator and they will help you out!'},
+            	{name: 'Extra Help', value: 'If no moderator assists you, then talk in general-chat for community support'}
+        	)
+        	.setImage('https://img.icons8.com/nolan/452/rocket-league.png')
+        	.setFooter('Rocket League Trading Server ©');
 
-        message.channel.send(newEmbed);
+			message.author.send(Embed);
+		break;
     }
 	}
 
