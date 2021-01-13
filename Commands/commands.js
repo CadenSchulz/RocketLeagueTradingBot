61c435c5a37412048b65e5705cabfd7944824b13
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-13 12:40:38 -0600

    Update commands.js

--- Commands/commands.js
+++ Commands/commands.js
@@ -7,8 +7,8 @@ module.exports = {
         .setTitle('**__List Of Commands__**')
         .setDescription('**Here is a complete and updated list of all commands:**')
         .addFields(
-            {name: '**-help**'},
-            {name: '**-youtube**', value: '**-youtube**'},
+            {name: '-help', value: '**-youtube**'},
+            {name: '-youtube', value: '**-youtube**'},
             {name: '**Others:**', value: 'Type the word "help"'}
         )
         .setFooter('Rocket League Trading Server ©');
