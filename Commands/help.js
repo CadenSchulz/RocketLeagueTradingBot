fdcc45ce21529ba436e21ed1dd6992a02f346c4d
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-17 12:45:59 -0600

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -6,8 +6,8 @@ module.exports = {
     execute(message, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#777777')
-        .setTitle('Hey,' + message.author.toString())
-        .setDescription('Check your messages!')
+        .setTitle('**__HELP CENTER__**')
+        .setDescription('**Check your messages!**')
         .setFooter('Rocket League Trading Server ©');
 
         message.channel.send(newEmbed);
