9e9f7fb51559d9d6f2c5fb72f107708954f0ff65
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-17 13:17:45 -0600

    Update yes.js

--- Commands/yes.js
+++ Commands/yes.js
@@ -4,14 +4,14 @@ module.exports = {
     execute(message, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#777777')
-        .setTitle('**__Getting you help__**')
-        .setDescription("Which of these things do you need help with?")
+        .setTitle('**__GETTING YOU HELP!__**')
+        .setDescription("**__Which of these things do you need help with?__**")
         .addFields(
-            {name: 'For help with scams and reporting players, please type:', value: '-scam'},
-            {name: 'For help with rules and guidelines of the server, please type:', value: '-rules'},
-            {name: 'For help with all commands of the server, please type:', value: '-commands'},
-            {name: 'For help or info about this bot, please type:', value: '-bot'},
-            {name: 'For any other help not listed here or to talk to a server representative, please type:', value: '-other'}
+            {name: 'For help with scams and reporting players, please type:', value: '**-scam**'},
+            {name: 'For help with rules and guidelines of the server, please type:', value: '**-rules**'},
+            {name: 'For help with all commands of the server, please type:', value: '**-commands**'},
+            {name: 'For help or info about this bot, please type:', value: '**-bot**'},
+            {name: 'For any other help not listed here or to talk to a server representative, please type:', value: '**-other**'}
         )
         .setFooter('Rocket League Trading Server ©');
 
