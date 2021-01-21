ea950225cf34ae5b69d2843cb94ec2a387e66b34
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-20 18:26:37 -0600

    Update shoutout.js

--- Commands/shoutout.js
+++ Commands/shoutout.js
@@ -7,7 +7,7 @@ module.exports = {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#777777')
         .setTitle('**Shoutout**')
-        .setDescription("**Shoutout to <@Sticky#4499> for this sick new server profile picture! Go follow him on instagram:** [Click Here](https://discord.gg/TwHsft5yhH).")
+        .setDescription("**Shoutout to @Sticky#4499 for this sick new server profile picture! Go join his discord server:** [Click Here](https://discord.gg/TwHsft5yhH).")
         .setImage('https://cdn.discordapp.com/attachments/800852669698539520/801519373574733914/image0.png')
         .setFooter('Rocket League Trading Server ©');
 
