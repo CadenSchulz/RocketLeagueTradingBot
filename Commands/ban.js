f74eaaa9c94d7280aaafeb71b88f51b136c29f0d
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-24 17:45:35 -0600

    Update ban.js

--- Commands/ban.js
+++ Commands/ban.js
@@ -3,8 +3,6 @@ module.exports = {
     description: "This command bans a member!",
     execute(message, args){
 
-        if(message.member.roles.cache.has('797392951310549033')){
-
         const target = message.mentions.users.first();
         if(target){
             const memberTarget = message.guild.members.cache.get(target.id);
@@ -12,9 +10,6 @@ module.exports = {
             message.channel.send("**User has been banned from Rocket League Trading Server**");
         }else{
             message.channel.send(`You do not have permission to ban that member!`);
-        } else{
-            message.channel.send('You do not have permission to use this command')
-        }
         }
     }
 }
