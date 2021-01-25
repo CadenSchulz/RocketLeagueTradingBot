c9486e3b0595f3a3d3d42283338929f3a43bc33d
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-24 22:52:14 -0600

    Update randomwinner.js

--- Commands/randomwinner.js
+++ Commands/randomwinner.js
@@ -9,7 +9,7 @@ module.exports = {
         .setColor('#777777')
         .setTitle('**__Winner of TW OCTANE__**')
         .addFields(
-            {name: '**Winner:**', value: `<@${529381901962706963}>`}
+            {name: '**Winner:**', value: `<@${AdoptMe#1467}>`}
         )
         .setFooter('Rocket League Trading Server ©');
 
