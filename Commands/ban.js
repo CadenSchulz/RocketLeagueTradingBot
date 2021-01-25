c0dbe89d3e8f52b1fa751b8deea0fb8bb9a9d843
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-24 18:00:22 -0600

    Update ban.js

--- Commands/ban.js
+++ Commands/ban.js
@@ -2,14 +2,13 @@ module.exports = {
     name: 'ban',
     description: "This command bans a member!",
     execute(message, args){
-
         const target = message.mentions.users.first();
-        if(target)
+        if(target){
             const memberTarget = message.guild.members.cache.get(target.id);
             memberTarget.kick();
-            message.channel.send("**User has been banned from Rocket League Trading Server**");
-        } else{
-            message.channel.send(`No member selected to ban (Please type: **-ban @______**)`);
+            message.channel.send("User has been banned from the Rocket League Trading Server");
+        }else{
+            message.channel.send(`You coudn't ban that member because no member was selected!`);
         }
     }
 }
