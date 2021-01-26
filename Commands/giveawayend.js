e41f5eadea72cf5801589774986870c25234f79a
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-26 10:50:51 -0600

    Update giveawayend.js

--- Commands/giveawayend.js
+++ Commands/giveawayend.js
@@ -8,10 +8,10 @@ module.exports = {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#777777')
         .setTitle('**__TW Octane Giveaway COMPLETE__**')
-        .setDescription('**This giveaway has ended. The owner of the server must follow these directions:**')
+        .setDescription('``This giveaway has ended. The owner of the server must follow these directions:``')
         .addFields(
-            {name: '**To select winner, type:**', value: '-randomwinner'},
-            {name: '**To delete this giveaway and start a new one, type:**', value: '-giveawaydelete'}
+            {name: '``To select winner, type:``', value: '-randomwinner'},
+            {name: '``To delete this giveaway and start a new one, type:``', value: '-giveawaydelete'}
         )
         .setFooter('Rocket League Trading Server ©');
 
