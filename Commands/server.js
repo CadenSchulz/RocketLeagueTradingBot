46a8f0924adae5b860039da0232838fb15aa43d0
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-26 07:43:36 -0600

    Update server.js

--- Commands/server.js
+++ Commands/server.js
@@ -1,5 +1,22 @@
-let embed = new Discord.MessageEmbed()
-            .setColor("RANDOM")
+// let embed = new Discord.MessageEmbed()
+//             .setColor("RANDOM")
+//             .setTitle("Server Info")
+//             .setImage(message.guild.iconURL)
+//             .setDescription(`${message.guild}'s information`)
+//             .addField("Owner", `The owner of this server is ${message.guild.owner}`)
+//             .addField("Member Count", `This server has ${message.guild.memberCount} members`)
+//             .addField("Emoji Count", `This server has ${message.guild.emojis.cache.size} emojis`)
+//             .addField("Roles Count", `This server has ${message.guild.roles.cache.size} roles`)
+            
+
+//         message.channel.send(embed)
+
+module.exports = {
+    name: 'scam',
+    description: "Embed for scam Command",
+    execute(message, args, Discord){
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
             .setTitle("Server Info")
             .setImage(message.guild.iconURL)
             .setDescription(`${message.guild}'s information`)
@@ -7,9 +24,10 @@ let embed = new Discord.MessageEmbed()
             .addField("Member Count", `This server has ${message.guild.memberCount} members`)
             .addField("Emoji Count", `This server has ${message.guild.emojis.cache.size} emojis`)
             .addField("Roles Count", `This server has ${message.guild.roles.cache.size} roles`)
-            
-
-        message.channel.send(embed)
+        
+        message.channel.send(newEmbed);
+    }
+}
 
 
 
