4cb4a63ea395ccd815212ee5e8405a1e71698541
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-26 10:57:09 -0600

    Update giveawayend.js

--- Commands/giveawayend.js
+++ Commands/giveawayend.js
@@ -12,8 +12,8 @@ module.exports = {
         .setThumbnail('https://temp-ca.s3.amazonaws.com/cdn-files/ris_ss788693510sst1611680127.png?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2YDWKLAVUAFT3HFY%2F20210126%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210126T165528Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Signature=819f667c3bcae58b2539e61e4c743091d3879a5116198b320e57cfc5a00cb19d')
         .setDescription('```This giveaway has ended. The owner of the server must follow these directions:```')
         .addFields(
-            {name: '``To select winner, type:``', value: '``-randomwinner``'},
-            {name: '``To delete this giveaway and start a new one, type:``', value: '``-giveawaydelete``'}
+            {name: '**To select winner, type:**', value: '-randomwinner'},
+            {name: '**To delete this giveaway and start a new one, type:**', value: '-giveawaydelete'}
         )
         .setFooter('Rocket League Trading Server ©');
 
