b1ae33b39a3d66b3a9ef8345b8e0f1618885b89a
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-25 21:15:49 -0600

    Update scam.js

--- Commands/scam.js
+++ Commands/scam.js
@@ -5,7 +5,7 @@ module.exports = {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#777777')
         .setTitle('**Scammers/Reporting Players**')
-        .setDescription("**To report a player of scamming or potentially scamming:** [click me](https://discord.gg/5v4NuMNeEH).")
+        .setDescription("**To report a player of scamming or potentially scamming:** <'800439439406923826'> [click me](https://discord.gg/5v4NuMNeEH).")
         .setFooter('Rocket League Trading Server ©');
 
         message.channel.send(newEmbed);
