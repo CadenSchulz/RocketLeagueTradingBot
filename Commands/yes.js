e796bbd1601a29a1127e6c9d1b7c779dbef267d1
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-26 14:50:37 -0600

    Update yes.js

--- Commands/yes.js
+++ Commands/yes.js
@@ -6,31 +6,32 @@ module.exports = {
         .setColor('#777777')
         .setTitle('**__GETTING YOU HELP!__**')
         .setDescription("**__Which of these things do you need help with?__**")
+        .setThumbnail(message.guild.iconURL())
         .addFields(
             {
                 name: 'For help with scams and reporting players, please type:', 
                 value: '**-scam**',
-                inline: true
+                inline: false
             },
             {
                 name: 'For help with rules and guidelines of the server, please type:', 
                 value: '**-rules**',
-                inline: true
+                inline: false
             },
             {
                 name: 'For help with all commands of the server, please type:',
                 value: '**-commands**',
-                inline: true
+                inline: false
             },
             {
                 name: 'For help or info about this bot, please type:',
                 value: '**-bot**',
-                inline: true
+                inline: false
             },
             {
                 name: 'For any other help not listed here or to talk to a server representative, please type:',
                 value: '**-other**',
-                inline: true
+                inline: false
             }
         )
         .setFooter('Rocket League Trading Server ©');
