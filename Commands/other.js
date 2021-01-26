2d0ad471e34552a6cc6cec5c34320ef8ca3419ca
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-25 21:27:29 -0600

    commands with #

--- Commands/other.js
+++ Commands/other.js
@@ -5,7 +5,7 @@ module.exports = {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#777777')
         .setTitle('**Other Help**')
-        .setDescription("**To talk to a representative:** [click me](https://discord.gg/R4WFMTtteS).")
+        .setDescription("**To talk to a representative:** <#797363588909891584>")
         .setFooter('Rocket League Trading Server ©');
 
         message.channel.send(newEmbed);
