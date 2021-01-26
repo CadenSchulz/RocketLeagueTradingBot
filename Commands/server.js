9654713e099fa23c4c47bfea5f7605a0acf1ecbf
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-26 07:53:41 -0600

    Update server.js

--- Commands/server.js
+++ Commands/server.js
@@ -1,5 +1,9 @@
-// let embed = new Discord.MessageEmbed()
-//             .setColor("RANDOM")
+// module.exports = {
+//     name: 'server',
+//     description: "Embed for server Command",
+//     execute(message, args, Discord){
+//         const newEmbed = new Discord.MessageEmbed()
+//         .setColor("777777")
 //             .setTitle("Server Info")
 //             .setImage(message.guild.iconURL)
 //             .setDescription(`${message.guild}'s information`)
@@ -7,27 +11,40 @@
 //             .addField("Member Count", `This server has ${message.guild.memberCount} members`)
 //             .addField("Emoji Count", `This server has ${message.guild.emojis.cache.size} emojis`)
 //             .addField("Roles Count", `This server has ${message.guild.roles.cache.size} roles`)
-            
+        
+//         message.channel.send(newEmbed);
+//     }
+// }
 
-//         message.channel.send(embed)
+
+
+
+
+
+
+
 
 module.exports = {
     name: 'server',
     description: "Embed for server Command",
     execute(message, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
-        .setColor("777777")
-            .setTitle("Server Info")
-            .setImage(message.guild.iconURL)
-            .setDescription(`${message.guild}'s information`)
-            .addField("Owner", `The owner of this server is ${message.guild.owner}`)
-            .addField("Member Count", `This server has ${message.guild.memberCount} members`)
-            .addField("Emoji Count", `This server has ${message.guild.emojis.cache.size} emojis`)
-            .addField("Roles Count", `This server has ${message.guild.roles.cache.size} roles`)
+        .setAuthor(message.guild.name, message.guild.iconURL)
+        .setColor(3447003)
+        .setDescription(`Owner: ${message.guild.owner.user.tag} (${message.guild.owner.id})`)
+        .addField('Member Count', `${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size} (${message.guild.members.filter(m=>m.user.bot).size} bots)`, true)
+        .addField('AFK Timeout', `${message.guild.afkTimeout / 60} minutes`, true)
+        .addField('AFK Channel', `${message.guild.afkChannelID === null ? 'No AFK Channel' : client.channels.get(message.guild.afkChannelID).name} (${message.guild.afkChannelID === null ? '' : message.guild.afkChannelID})`, true)
+        .addField('Location', message.guild.region, true)
+        .addField('Created', message.guild.createdAt.toLocaleString(), true)
+        .addBlankField(true)
+        .setTimestamp()
+        .setFooter(client.user.username, client.user.avatarURL);
         
         message.channel.send(newEmbed);
     }
 }
+
 
 
 
