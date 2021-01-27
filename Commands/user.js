0ecafb4938d51b16fb54623f190d4d02292ae0f0
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-27 15:45:11 -0600

    Update user.js

--- Commands/user.js
+++ Commands/user.js
@@ -70,56 +70,6 @@ module.exports = {
                     value: user.roles.cache.map(role => role.toString()).join(" ,"),
                     inline: true
                 }
-            );
-
-            const embed = new MessageEmbed()
-            .setTitle(`${user.user.username} stats`)
-            .setColor(`#f3f3f3`)
-            .setThumbnail(user.user.displayAvatarURL({dynamic : true}))
-            .addFields(
-                {
-                    name: ":information_source: **Name: **",
-                    value: user.user.username,
-                    inline: true
-                },
-                {
-                    name: "#️⃣ **Discriminator:** ",
-                    value: `#${user.user.discriminator}`,
-                    inline: true
-                },
-                {
-                    name: "🆔 **ID:** ",
-                    value: user.user.id,
-                },
-                {
-                    name: ":globe_with_meridians: **Current Status: **",
-                    value: status,
-                    inline: true
-                },
-                {
-                    name: ":eyes: **Activity:** ",
-                    value: user.presence.activities[0] ? user.presence.activities[0].name : `User isn't playing a game!`,
-                    inline: true
-                },
-                {
-                    name: ':link: **Avatar link:** ',
-                    value: `[Click Here](${user.user.displayAvatarURL()})`
-                },
-                {
-                    name: ':baby: **Creation Date: **',
-                    value: user.user.createdAt.toLocaleDateString("en-us"),
-                    inline: true
-                },
-                {
-                    name: ':calendar_spiral: **Joined Date: **',
-                    value: user.joinedAt.toLocaleDateString("en-us"),
-                    inline: true
-                },
-                {
-                    name: ':roll_of_paper: **User Roles: **',
-                    value: user.roles.cache.map(role => role.toString()).join(" ,"),
-                    inline: true
-                }
             )
 
         message.channel.send(embed)
