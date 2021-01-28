62eac26411e2e7cd9ce4a8db2cc5fce965e26618
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-27 23:46:20 -0600

    Update goodmorning.js

--- Commands/goodmorning.js
+++ Commands/goodmorning.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'good morning',
+    name: 'goodmorning',
     description: "Embed for good morning Command",
     execute(message, args, Discord){
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
