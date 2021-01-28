b860dbfe0ae10af3bc32d9a8f2736537d74e4d59
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-27 23:37:12 -0600

    Update gn.js

--- Commands/gn.js
+++ Commands/gn.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('#f3f3f3')
         .setTitle('**__Rocket League Trading Server__**')
         .setThumbnail('https://cdn.discordapp.com/attachments/700502377631055882/804221801433006110/gn.jpg')
-        .setDescription(`${user.user.username} **is going to bed...**`)
+        .setDescription(`${message.author} **is going to bed...**`)
         .setFooter('Rocket League Trading Server ©')
         .setTimestamp();
 
