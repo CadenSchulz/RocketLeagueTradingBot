e966a56f3209fd700e9b6875e14abcf2a99e2c39
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-28 10:11:09 -0600

    oldest

--- main.js
+++ main.js
@@ -121,6 +121,10 @@ client.on('message', message => {
 	if(command === 'goodnight'){
 		client.commands.get('goodnight').execute(message, args, Discord)
 	}
+	if(command === 'oldest'){
+		client.commands.get('oldest').execute(message, args, Discord)
+	}
+
 });
 
 client.login(process.env.token);
