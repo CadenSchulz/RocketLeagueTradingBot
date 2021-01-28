6aa7ce4a01b08afd5309e0aef09bb0a65a3587a5
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-28 08:07:14 -0600

    Update weather.js

--- Commands/weather.js
+++ Commands/weather.js
@@ -8,7 +8,7 @@ module.exports = {
 
     async run (client, message, args){
 
-    Weather.find({search: args.join(" "), degreeType: 'F'}, function (error, result){
+    weather.find({search: args.join(" "), degreeType: 'F'}, function (error, result){
         // 'C' can be changed to 'F' for farneheit results
         if(error) return message.channel.send(error);
         if(!args[0]) return message.channel.send('Please specify a location')
