9c807729acd262abbbb6625cca91a4297a61cf53
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-28 19:23:01 -0600

    Update announce.js

--- Commands/announce.js
+++ Commands/announce.js
@@ -7,7 +7,8 @@ module.exports = {
     let rChannel = message.guild.channels.cache.get(args[0])
     if (!rChannel)return message.channel.send(`You did not specify your channel to send the announcement to`)
     console.log(rChannel);
-    let MSG = message.content.split(`${bot.prefix}announce ${rChannel.id} `).join("")
+    let MSG = args.slice(1).join(" ");
+    // let MSG = message.content.split(`${bot.prefix}announce ${rChannel.id} `).join("")
     if (!MSG)return message.channel.send(`You did not specify your message to send!`)
     const _ = new MessageEmbed()
       .setTitle(`New announcement!`)
