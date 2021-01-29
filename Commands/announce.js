97a7e1c0a244963eb27998ff854f1b5ff04df27d
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-28 18:23:46 -0600

    Update announce.js

--- Commands/announce.js
+++ Commands/announce.js
@@ -4,12 +4,12 @@ module.exports = {
   description: "Get the bot to say what ever you want in a specific channel.",
   usage: "<channel id> <msg>",
   run: async (bot, message, args) => {
+    console.log(bot);
     let rChannel = message.mentions.channels.first() /* to allow the user to #channel instead of using the ID */ || bot.channels.cache.get(args[0]);
     if (!rChannel)
       return message.channel.send(
         `You did not specify your channel to send the announcement too!`
       );
-    console.log(bot);
     let MSG = message.content
       .split(`${bot.prefix}announce ${rChannel.id} `)
       .join("");
