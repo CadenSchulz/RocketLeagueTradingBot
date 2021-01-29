e511d0c5b641cf309029e62673af115cf4c3d267
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-28 18:43:31 -0600

    Update announce.js

--- Commands/announce.js
+++ Commands/announce.js
@@ -4,21 +4,15 @@ module.exports = {
   description: "Get the bot to say what ever you want in a specific channel.",
   usage: "<channel id> <msg>",
   run: async (bot, message, args) => {
-    let rChannel = message.guild.channels.cache.get(args[0]);
-    if (!rChannel)
-      return message.channel.send(
-        `You did not specify your channel to send the announcement too!`
-      );
+    let rChannel = message.guild.channels.cache.get(args[0])
+    if (!rChannel)return message.channel.send(`You did not specify your channel to send the announcement to`)
     console.log(rChannel);
-    let MSG = message.content
-      .split(`${bot.prefix}announce ${rChannel.id} `)
-      .join("");
-    if (!MSG)
-      return message.channel.send(`You did not specify your message to send!`);
+    let MSG = message.content.split(`${bot.prefix}announce ${rChannel.id} `).join("")
+    if (!MSG)return message.channel.send(`You did not specify your message to send!`)
     const _ = new MessageEmbed()
       .setTitle(`New announcement!`)
       .setDescription(`${MSG}`)
-      .setColor("RANDOM");
+      .setColor('#f3f3f3');
     rChannel.send(_);
     message.delete();
   },
