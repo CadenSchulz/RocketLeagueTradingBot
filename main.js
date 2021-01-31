c6fe0795ad0041d6a576eaa6caec675974da2252
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-31 10:36:22 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,23 +4,6 @@ const client = new Discord.Client();
 
 const newUsers = [];
 
-client.on("guildMemberAdd", (member) => {
-	const guild = member.guild;
-	if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-	newUsers[guild.id].set(member.id, member.user);
-  
-	if (newUsers[guild.id].size > 10) {
-	  const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-	  guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
-	  newUsers[guild.id].clear();
-	}
-  });
-  
-  client.on("guildMemberRemove", (member) => {
-	const guild = member.guild;
-	if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
-  });
-
 const privateMessag = require('./private-message')
 
 const prefix = '-';
