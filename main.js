1c069f5637aa1a1043b09b9a4e767318e12fb23e
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-31 10:38:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const newUsers = [];
+bot.on('guildMemberAdd', member => {
+    const exampleEmbed = new Discord.RichEmbed()
+        .setColor('#0099ff')
+        .setTitle('Welcome')
+        .addField('', member.nickname)
+        .setImage(member.user.avatarURL)
+
+    member.guild.channels.get('605000838573850635').send(exampleEmbed);
+})
 
 const privateMessag = require('./private-message')
 
