c6078ae523e6e99ddbc6f2dde91cca9085adf742
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-31 23:59:36 -0600

    Update twitch.js

--- Commands/twitch.js
+++ Commands/twitch.js
@@ -31,7 +31,7 @@ module.exports = {
             )
             .setFooter('Rocket League Trading Server ©');
 
-        message.channel.send(newEmbed).then(msg => {msg.react()})
+        message.channel.send(newEmbed).then(msg => {msg.react('👎')})
         // .then(() => message.react('👎'));
 
         const filter = (reaction, user) => {
