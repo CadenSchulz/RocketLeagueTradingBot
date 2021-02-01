081c6e869f9eab925825248802b1a34472a20af5
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-01-31 23:56:55 -0600

    Update twitch.js

--- Commands/twitch.js
+++ Commands/twitch.js
@@ -1,50 +1,47 @@
 module.exports = {
     name: 'twitch',
     description: "Embed for twitch Command",
-    execute(message, args, Discord){
+    execute(message, args, Discord) {
         const newEmbed = new Discord.MessageEmbed()
-        .setColor('#f3f3f3')
-        .setTitle('**__Rocket League Trading Server__**')
-        .setThumbnail('https://cdn.discordapp.com/icons/795513122362097675/8574e462fd6570678c1d763a3a0fa6c5.webp')
-        .setDescription("**This is the Rocket League Trading Server's Twitch Channel owned by <@700481325232160838>:** [click me](https://www.twitch.tv/customscrims_nae).")
-        .addFields(
-            {
-                name: ':one: Step One:', 
-                value: ':white_small_square:  **Follow**',
-                inline: false
-            },
-            {
-                name: ':two: Step Two:', 
-                value: ':white_small_square:  **Chat when live**',
-                inline: false
-            },
-            {
-                name: ':three: Step Three:',
-                value: ':white_small_square:  **Share The Stream With Friends and Family**',
-                inline: false
-            },
-            {
-                name: ':four: Step Four:',
-                value: ':white_small_square:  **Turn on Notifications**',
-                inline: false
-            }
+            .setColor('#f3f3f3')
+            .setTitle('**__Rocket League Trading Server__**')
+            .setThumbnail('https://cdn.discordapp.com/icons/795513122362097675/8574e462fd6570678c1d763a3a0fa6c5.webp')
+            .setDescription("**This is the Rocket League Trading Server's Twitch Channel owned by <@700481325232160838>:** [click me](https://www.twitch.tv/customscrims_nae).")
+            .addFields(
+                {
+                    name: ':one: Step One:',
+                    value: ':white_small_square:  **Follow**',
+                    inline: false
+                },
+                {
+                    name: ':two: Step Two:',
+                    value: ':white_small_square:  **Chat when live**',
+                    inline: false
+                },
+                {
+                    name: ':three: Step Three:',
+                    value: ':white_small_square:  **Share The Stream With Friends and Family**',
+                    inline: false
+                },
+                {
+                    name: ':four: Step Four:',
+                    value: ':white_small_square:  **Turn on Notifications**',
+                    inline: false
+                }
+            )
+            .setFooter('Rocket League Trading Server ©');
 
-            // {name: 'Step 1', value: 'Like'},
-            // {name: 'Step 2', value: 'Subscribe'},
-            // {name: 'Step 3', value: 'Comment'}
-        )
-        .setFooter('Rocket League Trading Server ©');
-
-        message.channel.send(newEmbed).then(() => message.react('👎'));
+        message.channel.send(newEmbed).then(msg => {msg.react()})
+        // .then(() => message.react('👎'));
 
         const filter = (reaction, user) => {
             return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
         };
-        
+
         message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
             .then(collected => {
                 const reaction = collected.first();
-        
+
                 if (reaction.emoji.name === '👍') {
                     message.reply('you reacted with a thumbs up.');
                 }
