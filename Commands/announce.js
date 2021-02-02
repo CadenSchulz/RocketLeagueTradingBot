e4056eefe703bb67726eb03de015bb315ef4235a
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-01 21:18:06 -0600

    e

--- Commands/announce.js
+++ Commands/announce.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "announce",
   description: "Get the bot to say what ever you want in a specific channel.",
   usage: "<channel> <msg>",
-  run: async (message, args, Discord) => {
+  execute: async (message, args, Discord) => {
     let rChannel = message.guild.channels.cache.get(args[0])
     if (!rChannel)return message.channel.send(`You did not specify your channel to send the announcement to`)
     console.log(rChannel);
--- Commands/eannounce.js
+++ Commands/eannounce.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "eannounce",
   description: "Get the bot to say what ever you want in a specific channel with @everyone.",
   usage: "<channel> <msg>",
-  run: async (message, args, Discord) => {
+  execute: async (message, args, Discord) => {
     let rChannel = message.guild.channels.cache.get(args[0])
     if (!rChannel)return message.channel.send(`You did not specify your channel to send the announcement to`)
     console.log(rChannel);
@@ -15,7 +15,7 @@ module.exports = {
       .setColor('#f3f3f3')
       .setTimestamp()
       .setFooter('Rocket League Trading Server ©')
-    rChannel.send('test', _);
+    rChannel.send('@everyone', _);
     message.delete();
   },
 };
