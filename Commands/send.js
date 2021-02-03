21d65191495e8ec66130ded803539d4fd348fdfc
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-02 19:53:53 -0600

    Update send.js

--- Commands/send.js
+++ Commands/send.js
@@ -1,8 +1,6 @@
-const { execute } = require("./ping");
-
 module.exports = {
     name: 'send',
-    description: 'sends image'
+    description: 'sends image',
     execute(message, args){
         message.channel.send('file:///C:/Users/caden/OneDrive/Pictures/Gaming/1.png');
     }
