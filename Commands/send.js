fdb9b9c75ca45451aeb4b1d9776bb85348d05be9
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-02 20:11:08 -0600

    Update send.js

--- Commands/send.js
+++ Commands/send.js
@@ -1,7 +1,9 @@
 module.exports = {
     name: 'image',
     description: 'sends image',
+    // execute(message, args){
+    //     message.channel.send(`https://i.ibb.co/c6FCkTv/1.pngle:///C:/Users/caden/OneDrive/Pictures/Gaming/1.png`);
     execute(message, args){
-        message.channel.send(`https://i.ibb.co/c6FCkTv/1.pngle:///C:/Users/caden/OneDrive/Pictures/Gaming/1.png`);
+        message.channel.send(`https://pasteboard.co/JMzKESj.png`);
     }
 }
