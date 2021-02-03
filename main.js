6479d382e7dc1078e59f9b017d85aea0c3c018ec
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-02 19:48:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const {Discord, Attachment} = require('discord.js');
+const Discord = require('discord.js');
 
-// const client = new Discord.Client();
-const client = new Client ();
+const client = new Discord.Client();
 
 const privateMessag = require('./private-message')
 
@@ -137,14 +136,6 @@ client.on('message', message => {
 	}
 	if(command === 'eannounce'){
 		client.commands.get('eannounce').execute(message, args, Discord);
-	}
-	let args = message.content.substring(prefix.length).split(" ");
-
-	switch(args[0]){
-		case 'send':
-			const attachment = new Attachment('file:///C:/Users/caden/OneDrive/Pictures/Gaming/1.png')
-			message.channel.send(message.author, attachment)
-		break;
 	}
 });
 
