b7c1ed18aced54b311d460c42a6fdec69b5d8f06
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-17 14:52:40 -0600

    giveaway

--- Commands/Giveaway.js
+++ Commands/Giveaway.js
@@ -7,14 +7,16 @@ module.exports = {
 
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#f3f3f3')
-        .setTitle('**__TW Octane Giveaway__**')
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+        .setTitle('**__Black Dieci (Exotic) Giveaway__**')
+        .setThumbnail('https://cdn.discordapp.com/attachments/804565322681286676/811698218701815808/dieci.png')
         .setDescription('**How To Enter:**')
         .addFields(
-            {name: '**Step 1**', value: 'React to this message (click the check mark)'},
-            {name: '**Step 2**', value: 'Invite as many people as you can to click the check mark!'},
-            {name: '**Step 3**', value: '__ONCE THIS MESSAGE HAS 700 REACTIONS (or check marks clicked) THEN THE OCTANE WILL BE GIVEN AT RANDOM TO A LUCKY WINNER__'}
+            {name: '**Step 1**', value: 'React to this message (click the check mark and you get 1 entry)'},
+            {name: '**Step 2**', value: 'For every person you invite, you get an extra entry (__**which means a better chance at winning!**__)'},
+            {name: '**Step 3**', value: '__A winner will be randomly chosen on **3/15/21**!__'}
         )
-        .setFooter('Rocket League Trading Server ©');
+        .setFooter('The winner is chosen completely randomly. You do not need to invite people to win, but it does help you. If you do not get randomly chosen and complain or accuse this bot as rigged, then it will result in a temporary mute. Thanks, Rocket League Trading Server ©');
 
 
             message.channel.send(newEmbed);
