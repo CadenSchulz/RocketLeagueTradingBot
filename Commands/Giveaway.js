8c67f68a63b0f4266175021d9f958cf6b5395cfe
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-17 15:04:43 -0600

    Update Giveaway.js

--- Commands/Giveaway.js
+++ Commands/Giveaway.js
@@ -16,7 +16,7 @@ module.exports = {
             {name: ':two:', value: 'For every person you invite, you get an extra entry (__**which means a better chance at winning!**__)'},
             {name: ':three:', value: '__A winner will be randomly chosen on **3/15/21**!__'}
         )
-        .setFooter(':asterisk: The winner is chosen completely randomly. You do not need to invite people to win, but it does help you. If you do not get randomly chosen and complain or accuse this bot as rigged, then it will result in a temporary mute. Thanks, Rocket League Trading Server ©');
+        .setFooter('🛈 The winner is chosen completely randomly. You do not need to invite people to win, but it does help you. If you do not get randomly chosen and complain or accuse this bot as rigged, then it will result in a temporary mute. Thanks, Rocket League Trading Server ©');
 
 
             message.channel.send(newEmbed);
