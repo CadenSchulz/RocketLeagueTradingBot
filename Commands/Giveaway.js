b9f4004d8ad82069f2624a17acbc032c1f300ed7
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-17 14:59:26 -0600

    emojis in giveaway

--- Commands/Giveaway.js
+++ Commands/Giveaway.js
@@ -12,9 +12,9 @@ module.exports = {
         .setThumbnail('https://cdn.discordapp.com/attachments/804565322681286676/811698218701815808/dieci.png')
         .setDescription('**How To Enter:**')
         .addFields(
-            {name: '**Step 1**', value: 'React to this message (click the check mark and you get 1 entry)'},
-            {name: '**Step 2**', value: 'For every person you invite, you get an extra entry (__**which means a better chance at winning!**__)'},
-            {name: '**Step 3**', value: '__A winner will be randomly chosen on **3/15/21**!__'}
+            {name: ':one:', value: 'React to this message (click the check mark and you get 1 entry)'},
+            {name: ':two:"', value: 'For every person you invite, you get an extra entry (__**which means a better chance at winning!**__)'},
+            {name: ':three:"', value: '__A winner will be randomly chosen on **3/15/21**!__'}
         )
         .setFooter('The winner is chosen completely randomly. You do not need to invite people to win, but it does help you. If you do not get randomly chosen and complain or accuse this bot as rigged, then it will result in a temporary mute. Thanks, Rocket League Trading Server ©');
 
