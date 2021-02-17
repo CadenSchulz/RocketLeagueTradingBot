5d3a6d3edba807b35c533235be1db742567a6393
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-17 15:12:13 -0600

    Update Giveaway.js

--- Commands/Giveaway.js
+++ Commands/Giveaway.js
@@ -6,19 +6,20 @@ module.exports = {
         if(message.member.roles.cache.has('795528794936311868')){
 
         const newEmbed = new Discord.MessageEmbed()
-        .setColor('#f3f3f3')
+        .setColor('#000000')
         .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
         .setTitle('**__Black Dieci (Exotic) Giveaway__**')
         .setThumbnail('https://cdn.discordapp.com/attachments/804565322681286676/811698218701815808/dieci.png')
         .setDescription('**How To Enter:**')
-        .addFields(
-            {name: ':one:', value: 'React to this message (click the check mark and you get 1 entry)'},
-            {name: ':two:', value: 'For every person you invite, you get an extra entry (__**which means a better chance at winning!**__)'},
-            {name: ':three:', value: '__A winner will be randomly chosen on **3/19/21**!__'}
-        )
-        .addFields(
-            {name: ':question: If you have any questions, please contact:', value: 'An owner or Moderator'}
-        )
+        .setDescription(':one: React to this message (click the check mark and you get 1 entry)')
+        // .addFields(
+        //     {name: ':one:', value: 'React to this message (click the check mark and you get 1 entry)'},
+        //     {name: ':two:', value: 'For every person you invite, you get an extra entry (__**which means a better chance at winning!**__)'},
+        //     {name: ':three:', value: '__A winner will be randomly chosen on **3/19/21**!__'}
+        // )
+        // .addFields(
+        //     {name: ':question: If you have any questions, please contact:', value: 'An owner or Moderator'}
+        // )
         .setFooter('🛈 The winner is chosen completely randomly. You do not need to invite people to win, but it does help you. If you do not get randomly chosen and complain or accuse this bot as rigged, then it will result in a temporary mute. Thanks, Rocket League Trading Server ©');
 
 
