f513f53b1b40011de86044fe68a6da9fd99bfb6d
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2021-02-17 15:18:08 -0600

    Update Giveaway.js

--- Commands/Giveaway.js
+++ Commands/Giveaway.js
@@ -11,7 +11,12 @@ module.exports = {
         .setTitle('**__Black Dieci (Exotic) Giveaway__**')
         .setThumbnail('https://cdn.discordapp.com/attachments/804565322681286676/811698218701815808/dieci.png')
         .setDescription('**How To Enter:**')
-        .setDescription(':one: React to this message (click the check mark and you get 1 entry)')
+        // .setDescription(':one: React to this message (click the check mark and you get 1 entry)')
+
+
+
+
+
         // .addFields(
         //     {name: ':one:', value: 'React to this message (click the check mark and you get 1 entry)'},
         //     {name: ':two:', value: 'For every person you invite, you get an extra entry (__**which means a better chance at winning!**__)'},
@@ -20,6 +25,20 @@ module.exports = {
         // .addFields(
         //     {name: ':question: If you have any questions, please contact:', value: 'An owner or Moderator'}
         // )
+
+
+
+
+        .addFields(
+            { name: 'Regular field title', value: 'Some value here' },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+        )
+
+
+
+
         .setFooter('🛈 The winner is chosen completely randomly. You do not need to invite people to win, but it does help you. If you do not get randomly chosen and complain or accuse this bot as rigged, then it will result in a temporary mute. Thanks, Rocket League Trading Server ©');
 
 
